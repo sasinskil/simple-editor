a21e0e09364db0f54fac28adaf4c3dacd6554d88
sasinskil <sasinskil@gmail.com>
2019-07-21 15:15:15 +0200

    saving the massage in localStorage - event handling

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -16,7 +16,45 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+const message = {
+  title: '',
+  content: ''
+}
 
-console.log(`Hello world!`);
+const title = document.querySelector('.form__title--input-js');
+const content = document.querySelector('.form__content--input-js');
+const btnSave = document.querySelector('.form__buttons--save-js');
+const btnLoad = document.querySelector('.form__buttons--load-js');
+
+btnSave.addEventListener('click', () => {
+
+  if (title.value.length > 0 && content.value.length > 0) {
+    message.title = title.value;
+    message.content = content.value;
+
+    const JsonMessage = JSON.stringify(message);
+    localStorage.setItem('message', JsonMessage);
+
+    title.value = "";
+    content.value = "";
+  } else {
+    alert('Fill in the fields first !');
+  }
+})
+
+btnLoad.addEventListener('click', () => {
+  if (localStorage.getItem('message')) {
+
+    const messageLocalStorage = localStorage.getItem('message')
+
+    const newMessage = JSON.parse(messageLocalStorage);
+
+    title.value = newMessage.title;
+    content.value = newMessage.content;
+
+  } else {
+    alert('You need to create your message first !');
+  }
+})
 
 
